18b6925894260b562000df04091e00c48d0a0049
Abhishek PM <codeabhishek18@gmail.com>
2024-08-26 09:57:19 +0530

    First commit

---
+++ app/api/course/[courseId]/route.js
@@ -0,0 +1,21 @@
+import dbConnect from "@/dbConfig/dbConnect";
+import { Course } from "@/models/course.model";
+import { Lecture } from "@/models/lecture.model";
+import { NextResponse } from "next/server";
+
+export async function GET(req, {params})
+{
+    try
+    {
+        await dbConnect();
+        const { courseId } = params;
+        const course = await Course.findOne({id: courseId});
+        if(!course)
+            throw new Error('Course not found');
+        return NextResponse.json(course);
+    }
+    catch(error)
+    {
+        return NextResponse.json({error: error.message});
+    }
+}
